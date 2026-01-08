e7af7f5a307ee98ab3593ccb7cb4070e70717778
tinodin <constantin.schlueter@icloud.com>
2026-01-08 22:51:14 +0100

    Changing PnPCapabilities is not needed because it affects the settings that are already set above

--- Assets/Scripts/networkpowermanagement.ps1
+++ Assets/Scripts/networkpowermanagement.ps1
@@ -51,15 +51,3 @@ Get-WmiObject -Class Win32_NetworkAdapter | Where-Object { $_.PNPDeviceID -like 
         }
     }
 }
-
-# Disable PnP Capabilities
-Get-WmiObject -Class Win32_NetworkAdapter | Where-Object { $_.PNPDeviceID -like "PCI\VEN_*" } | ForEach-Object {
-    $pnpDeviceId = $_.PNPDeviceID
-    $regPath = "HKLM:\SYSTEM\CurrentControlSet\Enum\$pnpDeviceId"
-    $driver = (Get-ItemProperty -Path $regPath -Name "Driver" -ErrorAction SilentlyContinue).Driver
-    $classKey = "HKLM:\SYSTEM\CurrentControlSet\Control\Class\$driver"
-    $physicalMediaType = (Get-ItemProperty -Path $classKey -Name "*PhysicalMediaType" -ErrorAction SilentlyContinue)."*PhysicalMediaType"
-    if ($physicalMediaType -eq "14" -or $physicalMediaType -eq "9") {
-        Set-ItemProperty -Path $classKey -Name "PnPCapabilities" -Value 24 -Type DWord -Force
-    }
-}
