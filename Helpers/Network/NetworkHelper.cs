39c9e02ad56c67ab5c0d57dc4b34bbc9126f9580
tinodin <constantin.schlueter@icloud.com>
2026-04-05 14:45:37 +0200

    Add more advanced network adapter settings

--- Helpers/Network/NetworkHelper.cs
+++ Helpers/Network/NetworkHelper.cs
@@ -151,38 +151,62 @@ public static class NetworkHelper
 
         if (device.NicType == NicDeviceType.WiFi)
         {
+            anyChanged |= ApplySetting(device, settings, "2.4G Wireless Mode", "IEEE 802.11b/g/n/ax");
+            anyChanged |= ApplySetting(device, settings, "5G Wireless Mode", "IEEE 802.11a/n/ac/ax");
+            anyChanged |= ApplySetting(device, settings, "802.11ax/ac/n/abg", "1. 802.11ax");
             anyChanged |= ApplySetting(device, settings, "802.11a/b/g Wireless Mode", "6. Dual Band 802.11a/b/g");
             anyChanged |= ApplySetting(device, settings, "802.11be/ax/ac/n/abg", "1. 802.11be");
+            anyChanged |= ApplySetting(device, settings, "802.11d", "Disabled");
             anyChanged |= ApplySetting(device, settings, "802.11/ac/ax Wireless Mode", "4. 802.11ax");
+            anyChanged |= ApplySetting(device, settings, "802.11n channel width for 2.4GHz", "Auto");
+            anyChanged |= ApplySetting(device, settings, "802.11n channel width for 5.2GHz", "Auto");
             anyChanged |= ApplySetting(device, settings, "802.11n/ac Wireless Mode", "802.11ac");
             anyChanged |= ApplySetting(device, settings, "ARP offload for WoWLAN", "Enabled");
+            anyChanged |= ApplySetting(device, settings, "Band Selection", "1. All Band");
+            anyChanged |= ApplySetting(device, settings, "Beacon Interval", "100");
             anyChanged |= ApplySetting(device, settings, "Channel Width for 2.4GHz", "Auto");
             anyChanged |= ApplySetting(device, settings, "Channel Width for 5GHz", "Auto");
             anyChanged |= ApplySetting(device, settings, "Channel Width for 6GHz", "Auto");
             anyChanged |= ApplySetting(device, settings, "D0 PacketCoalescing", "Disable");
+            anyChanged |= ApplySetting(device, settings, "EnableAdaptivity", "Auto");
             anyChanged |= ApplySetting(device, settings, "Fat Channel Intolerant", "Disabled");
             anyChanged |= ApplySetting(device, settings, "Global BG Scan blocking", "Always");
             anyChanged |= ApplySetting(device, settings, "GTK rekeying for WoWLAN", "Disabled");
+            anyChanged |= ApplySetting(device, settings, "HLDiffForAdaptivity", "7");
+            anyChanged |= ApplySetting(device, settings, "HT mode", "VHT mode");
             anyChanged |= ApplySetting(device, settings, "Idle Power Down Restriction", "Enabled");
+            anyChanged |= ApplySetting(device, settings, "L2HForAdaptivity", "Auto");
             anyChanged |= ApplySetting(device, settings, "MIMO Power Save Mode", "No SMPS");
             anyChanged |= ApplySetting(device, settings, "Mixed Mode Protection", "CTS-to-self Enabled");
+            anyChanged |= ApplySetting(device, settings, "Multi-Channel Concurrent", "Enabled + Hotspot");
             anyChanged |= ApplySetting(device, settings, "NS offload for WoWLAN", "Enabled");
             anyChanged |= ApplySetting(device, settings, "Packet Coalescing", "Disabled");
+            anyChanged |= ApplySetting(device, settings, "Power Saving", "Disabled");
             anyChanged |= ApplySetting(device, settings, "Preferred Band", "1. No Preference");
+            anyChanged |= ApplySetting(device, settings, "Preamble Mode", "Short & long");
+            anyChanged |= ApplySetting(device, settings, "QoS Support", "Auto");
             anyChanged |= ApplySetting(device, settings, "Roaming Aggressiveness", "1. Lowest");
+            anyChanged |= ApplySetting(device, settings, "Roaming Aggressiveness", "1. Disabled");
             anyChanged |= ApplySetting(device, settings, "Roaming aggressiveness", "1.Lowest");
+            anyChanged |= ApplySetting(device, settings, "Roaming Sensitivity Level", "Disable");
             anyChanged |= ApplySetting(device, settings, "Sleep on WoWLAN Disconnect", "Disabled");
             anyChanged |= ApplySetting(device, settings, "Throughput Booster", "Disabled");
             anyChanged |= ApplySetting(device, settings, "Transmit Power", "5. Highest");
+            anyChanged |= ApplySetting(device, settings, "Transmit Power Level", "1. Highest");
+            anyChanged |= ApplySetting(device, settings, "U-APSD Support", "Disabled");
             anyChanged |= ApplySetting(device, settings, "U-APSD support", "Disabled");
+            anyChanged |= ApplySetting(device, settings, "USB Mode", "Auto");
             anyChanged |= ApplySetting(device, settings, "Ultra High Band (6GHz)", "Enabled");
+            anyChanged |= ApplySetting(device, settings, "VHT 2.4G", "Enable");
             anyChanged |= ApplySetting(device, settings, "Wake on Magic Packet", "Disabled");
             anyChanged |= ApplySetting(device, settings, "WakeOnMagicPacket", "Disable");
             anyChanged |= ApplySetting(device, settings, "Wake on Pattern Match", "Disabled");
             anyChanged |= ApplySetting(device, settings, "WakeOnPatternMatch", "Disable");
+            anyChanged |= ApplySetting(device, settings, "Wireless Mode", "6. 802.11a/b/g");
         }
         else if (device.NicType == NicDeviceType.LAN)
         {
+            anyChanged |= ApplySetting(device, settings, "Adaptive Inter-Frame Spacing", "Disabled");
             anyChanged |= ApplySetting(device, settings, "Advanced EEE", "Disabled");
             anyChanged |= ApplySetting(device, settings, "ARP Offload", "Enabled");
             anyChanged |= ApplySetting(device, settings, "Auto Disable Gigabit", "Disabled");
@@ -195,6 +219,7 @@ public static class NetworkHelper
             anyChanged |= ApplySetting(device, settings, "Flow Control", "Disabled");
             anyChanged |= ApplySetting(device, settings, "Gigabit Lite", "Disabled");
             anyChanged |= ApplySetting(device, settings, "Gigabit Master Slave Mode", "Auto Detect");
+            anyChanged |= ApplySetting(device, settings, "Gigabit PHY Mode", "Auto Detect");
             anyChanged |= ApplySetting(device, settings, "Green Ethernet", "Disabled");
             anyChanged |= ApplySetting(device, settings, "Idle power down restriction", "Enabled");
             if (settings.Any(s => s.Name == "Interrupt Moderation Rate"))
@@ -215,15 +240,17 @@ public static class NetworkHelper
             anyChanged |= ApplySetting(device, settings, "Link Speed Battery Saver", "Disabled");
             anyChanged |= ApplySetting(device, settings, "Locally Administered Address", "--");
             anyChanged |= ApplySetting(device, settings, "Log Link State Event", "Disabled");
-            anyChanged |= ApplySetting(device, settings, "Maximum Number of RSS Queues", "4 Queues");
+            //anyChanged |= ApplySetting(device, settings, "Maximum Number of RSS Queues", "4 Queues");
             anyChanged |= ApplySetting(device, settings, "Multi-Channel Concurrent", "Disabled");
             anyChanged |= ApplySetting(device, settings, "NS Offload", "Enabled");
             anyChanged |= ApplySetting(device, settings, "Packet Priority & VLAN", "Packet Priority & VLAN Enabled");
             anyChanged |= ApplySetting(device, settings, "PCI Express Link Power Saving", "Disabled");
+            anyChanged |= ApplySetting(device, settings, "Power Saving", "Disabled");
             anyChanged |= ApplySetting(device, settings, "Power Saving Mode", "Disabled");
             anyChanged |= ApplySetting(device, settings, "Preamble Mode", "Short");
             anyChanged |= ApplySetting(device, settings, "Protocol ARP Offload", "Enabled");
             anyChanged |= ApplySetting(device, settings, "Protocol NS Offload", "Enabled");
+            anyChanged |= ApplySetting(device, settings, "PTP Hardware Timestamp", "Disabled");
             anyChanged |= ApplySetting(device, settings, "Recv Segment Coalescing (IPv4)", "Enabled");
             anyChanged |= ApplySetting(device, settings, "Recv Segment Coalescing (IPv6)", "Enabled");
             anyChanged |= ApplySetting(device, settings, "Receive Side Scaling", "Disabled");
@@ -231,7 +258,8 @@ public static class NetworkHelper
             anyChanged |= ApplySetting(device, settings, "Selective Suspend", "Disabled");
             anyChanged |= ApplySetting(device, settings, "SelectiveSuspend", "Disabled");
             anyChanged |= ApplySetting(device, settings, "Selective Suspend Idle Timeout", "60");
-            anyChanged |= ApplySetting(device, settings, "Shutdown Wake-On-Lan", "Disabled");
+            anyChanged |= ApplySetting(device, settings, "Software Timestamp ", "Disabled");
+            //anyChanged |= ApplySetting(device, settings, "Shutdown Wake-On-Lan", "Disabled");
             anyChanged |= ApplySetting(device, settings, "Speed & Duplex", "Auto Negotiation");
             anyChanged |= ApplySetting(device, settings, "System Idle Power Saver", "Disabled");
             anyChanged |= ApplySetting(device, settings, "TCP Checksum Offload (IPv4)", "Rx & Tx Enabled");
@@ -250,8 +278,8 @@ public static class NetworkHelper
             // anyChanged |= ApplySetting(device, settings, "Wake on Magic Packet", "Disabled");
             // anyChanged |= ApplySetting(device, settings, "Wake On Magic Packet From S5", "Disabled");
             // anyChanged |= ApplySetting(device, settings, "Wake on magic packet when system is in the S0ix power state", "Disabled");
-             anyChanged |= ApplySetting(device, settings, "Wake on Pattern Match", "Disabled");
-             anyChanged |= ApplySetting(device, settings, "Wake on pattern match", "Disabled");
+            anyChanged |= ApplySetting(device, settings, "Wake on Pattern Match", "Disabled");
+            anyChanged |= ApplySetting(device, settings, "Wake on pattern match", "Disabled");
             anyChanged |= ApplySetting(device, settings, "Wake on Ping", "Disabled");
             anyChanged |= ApplySetting(device, settings, "WOL & Shutdown Link Speed", "Not Speed Down");
             anyChanged |= ApplySetting(device, settings, "WOL Link Power Saving", "Disabled");
