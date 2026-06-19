c27c2043148d7698eca376f011b4d4298700e16c
tinodin <constantin.schlueter@icloud.com>
2026-06-19 19:33:04 +0200

    Remove Legacy Context Menu option

--- src/AutoOS.App/Views/Updater/Stages/UpdateStage.cs
+++ src/AutoOS.App/Views/Updater/Stages/UpdateStage.cs
@@ -1,4 +1,6 @@
+using AutoOS.Core.Helpers.Registry;
 using Microsoft.Win32;
+using System.Diagnostics;
 
 namespace AutoOS.Views.Updater.Stages;
 
@@ -9,7 +11,10 @@ public static class UpdateStage
 		var actions = new List<(string Title, Func<Task> Action, Func<bool> Condition)>
 		{
 			// disable legacy context menu
-			("Disable Legacy Context Menu", async () => Registry.CurrentUser.DeleteSubKeyTree(@"Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}", false), null)
+			("Disable Legacy Context Menu", async () => Registry.CurrentUser.DeleteSubKeyTree(@"Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}", false), null),
+			("Disable Legacy Context Menu", async () => await Process.Start(new ProcessStartInfo { FileName = "reg.exe", Arguments = $@"load HKU\DefaultUser ""{Path.Combine(Path.GetPathRoot(Environment.SystemDirectory)!, "Users", "Default", "NTUSER.DAT")}""", CreateNoWindow = true })!.WaitForExitAsync(), null),
+			("Disable Legacy Context Menu", async () => RegistryHelper.SetValue(RegistryHelper.Identity.CurrentUser, @"HKEY_CURRENT_USER\Software\StartIsBack", "NoXAMLMenus", 0, RegistryValueKind.DWord, true), null),
+			("Disable Legacy Context Menu", async () => await Process.Start(new ProcessStartInfo { FileName = "reg.exe", Arguments = @"unload HKU\DefaultUser", CreateNoWindow = true })!.WaitForExitAsync(), null)
 		};
 
 		return actions;
